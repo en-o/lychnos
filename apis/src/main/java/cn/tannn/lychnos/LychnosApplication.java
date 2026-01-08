9138735407ff5c8e262b2686471adf9dcf5cd50b
tan <13320385140@163.com>
2026-01-08 21:50:15 +0800

    init api

--- apis/src/main/java/cn/tannn/lychnos/LychnosApplication.java
+++ apis/src/main/java/cn/tannn/lychnos/LychnosApplication.java
@@ -1,9 +1,11 @@
 package cn.tannn.lychnos;
 
+import cn.tannn.jdevelops.autoschema.scan.EnableAutoSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoSchema
 public class LychnosApplication {
 
     public static void main(String[] args) {
