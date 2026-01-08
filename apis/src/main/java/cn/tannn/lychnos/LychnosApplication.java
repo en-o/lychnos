e9a4887522edc9ac802860fe8d60d72ca59f7923
tan <13320385140@163.com>
2026-01-08 21:17:25 +0800

    init api

---
+++ apis/src/main/java/cn/tannn/lychnos/LychnosApplication.java
@@ -0,0 +1,13 @@
+package cn.tannn.lychnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LychnosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LychnosApplication.class, args);
+    }
+
+}
---
+++ apis/src/test/java/cn/tannn/lychnos/LychnosApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tannn.lychnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LychnosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
