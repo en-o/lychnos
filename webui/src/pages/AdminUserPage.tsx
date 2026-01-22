e786dfe257b0bae689dde09fc18bc62ad23d4ea9
tan <13320385140@163.com>
2026-01-22 13:23:11 +0800

    fix

--- webui/src/pages/AdminUserPage.tsx
+++ webui/src/pages/AdminUserPage.tsx
@@ -1,7 +1,6 @@
 import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import {adminApi, type UserDetail, type ThirdPartyBind} from '../api/admin';
-import {toast} from '../components/ToastContainer';
 
 function AdminUserPage() {
     const navigate = useNavigate();
